637d694d159ca0cdd3e86d19b3916f11c4ba478b
theonlyone <theovitacco@gmail.com>
2022-04-23 11:09:48 -0300

    video 2 done

--- Assets/scripts/MayorDeEdad.cs
+++ Assets/scripts/MayorDeEdad.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class MayorDeEdad : MonoBehaviour
 {
+    public int edadUsuario;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (edadUsuario >= 18)
+        {
+            Debug.Log("es mayor de edad");
+        }
+        else
+        {
+            Debug.Log("no es mayor de edad");
+        }
     }
 
     // Update is called once per frame
