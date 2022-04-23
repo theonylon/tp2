7dba730931ed2a0ae885b8d91ac621abd72ed41b
theonlyone <theovitacco@gmail.com>
2022-04-23 12:01:28 -0300

    video 5 and 6 done

--- Assets/scripts/validarContraseña.cs
+++ Assets/scripts/validarContraseña.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI; 
 public class validarContraseña : MonoBehaviour
 {
+    string contraseniacorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
     // Start is called before the first frame update
     void Start()
     {
-        
+        contraseniacorrecta = "12345";
     }
 
     // Update is called once per frame
@@ -17,6 +20,16 @@ public class validarContraseña : MonoBehaviour
     }
     public void validarContrasenia()
     {
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniacorrecta)
+        {
+            Debug.Log("bienvenido!");
 
+        }
+
+        else
+        {
+            Debug.Log("contraseña incorrecta");
+        }
     }
 }
